7219218cb182079647949f7a63b7e4d5a571a66a
chronosxvi <arvin.peralta@gmail.com>
2018-03-23 11:10:26 +0800

    installed carrierwave

---
+++ app/models/member.rb
@@ -0,0 +1,8 @@
+class Member < ApplicationRecord
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
+
+end
