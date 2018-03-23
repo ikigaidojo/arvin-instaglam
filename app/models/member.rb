b4752534527a5924018ce8dc8e2318b2f387bbd1
chronosxvi <arvin.peralta@gmail.com>
2018-03-23 12:55:51 +0800

    installed refile

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,3 +6,7 @@ class Member < ApplicationRecord
   validates :email, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
 
 end
+
+class User < ActiveRecord::Base #Use Refile in a Model
+  attachment :profile_image
+end
